6ba9678dae28ade7647db551461e5f657d32e26a
Tony Cai <081099@gmail.com>
2017-07-18 15:07:22 -0700

    Add team signup logic, and tag parsing

--- store/teams.go
+++ store/teams.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/boltdb/bolt"
 )
@@ -40,4 +42,44 @@ func AddTeamIfNotExists(teamname string) error {
 		_, err := teams.CreateBucketIfNotExists([]byte(teamname))
 		return err
 	})
+}
+
+func ListTeamTags(teamname string) (map[string]int, error) {
+	tags := make(map[string]int)
+
+	err := db.View(func(tx *bolt.Tx) error {
+		teams := tx.Bucket([]byte("teams"))
+		if teams == nil {
+			return errors.New("'teams' bucket does not exist")
+		}
+		team := teams.Bucket([]byte(teamname))
+
+		// for each user
+		c := team.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// fetch the user's tags
+			if v == nil { // if this is a bucket, it must be a user
+				user := team.Bucket(k)
+				var usertags []string
+				if exists := user.Get([]byte("self-tags")); exists != nil {
+					usertags = strings.Split(fmt.Sprintf("%s", exists), ",")
+				}
+				if exists := user.Get([]byte("search-tags")); exists != nil {
+					usertags = append(usertags, strings.Split(fmt.Sprintf("%s", exists), ",")...)
+				}
+
+				// add to tags list
+				for _, tag := range usertags {
+					if count, ok := tags[tag]; !ok {
+						tags[tag] = 1
+					} else {
+						tags[tag] = count + 1
+					}
+				}
+			}
+		}
+		return nil
+	})
+
+	return tags, err
 }
