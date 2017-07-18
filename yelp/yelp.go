9808e3165e5797978709d6b862b65da8cf9b8a72
Tony Cai <081099@gmail.com>
2017-07-17 18:38:50 -0700

    Initialize yelp package skeleton

--- yelp/yelp.go
+++ yelp/yelp.go
@@ -1,1 +1,27 @@
 package yelp
+
+import (
+	"strings"
+	"time"
+)
+
+var (
+	AccessToken string
+)
+
+type Location struct {
+	city string
+	long float64
+	lat  float64
+}
+
+func GetSuggestions(loc Location, t time.Time, opts []string) ([]string, error) {
+	AccessToken = "abc"
+	if strings.HasPrefix(AccessToken, "vault:v1") {
+		// use vault to decipher access token
+	}
+
+	// fetch list of resturants from yelp API with access token
+
+	return []string{"starbucks", "mcdonalds", "tim hortons"}, nil
+}
