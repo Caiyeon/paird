fb1b1e43fb112a336958952d1ab33dc6ec81ea03
Mai <henry.mai@sap.com>
2017-07-18 12:13:59 -0700

    Added Frontend files

---
+++ frontend/src/components/contactus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ContactUs">
+    <h1>This is theContacts Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contactus'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
