d286d27c27bfa0a39a8f392046ca41b93d4966b9
Tony Cai <081099@gmail.com>
2017-07-18 13:40:42 -0700

    Add help message to interactive message

--- handlers/interactive.go
+++ handlers/interactive.go
@@ -68,44 +68,88 @@ func Interactive() echo.HandlerFunc {
 			} else {
 				text = "Ok, you don't mind being either"
 			}
+			text = text + "\nAre you looking for a:"
+
 			// insert user's self tag
-		default:
-			// do nothing
-		}
 
-		text = text + "\nAre you looking for a:"
-
-		return c.JSON(http.StatusOK, H{
-			"text": "Step 2:",
-			"attachments": []interface{}{
-				map[string]interface{}{
-					"text":            text,
-					"fallback":        "Unsupported slack client",
-					"callback_id":     "signup_seek",
-					"color":           "#3AA3E3",
-					"attachment_type": "default",
-					"actions": []interface{}{
-						map[string]string{
-							"name":  "seek",
-							"text":  "Mentor",
-							"type":  "button",
-							"value": "mentor",
-						},
-						map[string]string{
-							"name":  "seek",
-							"text":  "Mentee",
-							"type":  "button",
-							"value": "mentee",
-						},
-						map[string]string{
-							"name":  "seek",
-							"text":  "Doesn't matter",
-							"type":  "button",
-							"value": "",
+			return c.JSON(http.StatusOK, H{
+				"text": "Step 2:",
+				"attachments": []interface{}{
+					map[string]interface{}{
+						"text":            text,
+						"fallback":        "Unsupported slack client",
+						"callback_id":     "signup_seek",
+						"color":           "#3AA3E3",
+						"attachment_type": "default",
+						"actions": []interface{}{
+							map[string]string{
+								"name":  "seek",
+								"text":  "Mentor",
+								"type":  "button",
+								"value": "mentor",
+							},
+							map[string]string{
+								"name":  "seek",
+								"text":  "Mentee",
+								"type":  "button",
+								"value": "mentee",
+							},
+							map[string]string{
+								"name":  "seek",
+								"text":  "Doesn't matter",
+								"type":  "button",
+								"value": "",
+							},
 						},
 					},
 				},
-			},
-		})
+			})
+
+		case "signup_seek":
+			if payload.Actions[0].Value == "mentor" || payload.Actions[0].Value == "mentee" {
+				text = "Ok, you are seeking a " + payload.Actions[0].Value
+			} else {
+				text = "Ok, you don't mind meeting up with a mentor or mentee"
+			}
+			return c.JSON(http.StatusOK, H{
+				"text":        text + "\nDone! You will be notified when a pairing is made!\nTo enhance your experience:",
+				"attachments": helpMessage,
+			})
+
+		default:
+			// no callback found, return nothing in fallthrough
+		}
+
+		return c.JSON(http.StatusOK, H{})
 	}
+}
+
+var helpMessage = []interface{}{
+	map[string]interface{}{
+		"title":           "Add some tags to yourself",
+		"text":            "`/listtags` _returns a list of popular tags in your team_\n`/addmytags [business, software development, ...]` _describe yourself!_\n`/addsearchtags [marketing, ...]` _what are you looking for specifically?_\n`/clearalltags` _clears your own tags and search tags_",
+		"mrkdwn_in":       []string{"text"},
+		"fallback":        "Unsupported slack client",
+		"callback_id":     "signup_self",
+		"color":           "#3AA3E3",
+		"attachment_type": "default",
+	},
+	map[string]interface{}{
+		"title":           "Add meetup preferences",
+		"text":            "`/mydiet [none|vegetarian|vegan]` _add dietary restrictions_\n`/myavailability [9AM-2PM] [Monday,Tuesday,Wednesday]` _restrict meetup times_\n`/mylocation [Vancouver]` _set your location for meetup suggestions_",
+		"mrkdwn_in":       []string{"text"},
+		"fallback":        "Unsupported slack client",
+		"callback_id":     "signup_self",
+		"color":           "#008000",
+		"attachment_type": "default",
+	},
+	map[string]interface{}{
+		"title":           "To see these options again",
+		"text":            "`/paird` _displays help message_",
+		"mrkdwn_in":       []string{"text"},
+		"fallback":        "Unsupported slack client",
+		"callback_id":     "signup_self",
+		"color":           "#DAF7A6",
+		"attachment_type": "default",
+	},
 }
