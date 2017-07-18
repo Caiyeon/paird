39b955cbcdf1283061276b2de55df77883cbf2e2
Yonni Luu <theahbooboo@Yonnis-MBP.dhcp.van.sap.corp>
2017-07-18 12:07:23 -0700

    added encrypt and decrypt function for vault

---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+"github.com/caiyeon/lunch-with-us/vault"
+
+
+)
+// main page to test functions
+func main(){
+vault.Decrypt("vault:v1:oXS9ezMRv6UFulP9n2q4DSZ2WPc9JyuSTTzEbByXaGg=")
+}
--- vault/vault.go
+++ vault/vault.go
@@ -3,23 +3,69 @@ package vault
 import (
 	"errors"
 	"strings"
+	"net/http"
+	"log"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+
 )
 
 const VaultAddr = "https://vault-ui.io"
 
-var VaultToken string
+var VaultToken="ca13caaa-6ff6-a6e1-8770-e0b107719a8b"
+type encryptData struct {
+	Result string `json:"result"`
+}
 
 // given a string, encrypt it with vault and return the cipher
-func Encrypt(plaintext string) (string, error) {
+func Encrypt(text string) (string, error) {
 	// make call to vault-ui.io
-	return "", nil
+	client := &http.Client{}
+	payload := strings.NewReader("key=lwu&plaintext="+ text)
+	req, err := http.NewRequest("POST", "https://vault-ui.io/api/transit/encrypt", payload)
+    if err != nil {
+        log.Print(err)
+    }
+    req.Header.Add("X-Vault-Token", VaultToken)
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+    resp, err := client.Do(req)
+    defer resp.Body.Close()
+    fmt.Println("Status is ",resp.Status)
+    respString, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("Response: ", string(respString))
+
+    // extact response string
+    stripped := encryptData{}
+    json.Unmarshal([]byte(respString),&stripped)
+    fmt.Println("Encryption token is: ",stripped.Result)
+	return stripped.Result, nil
 }
 
 // given a string, decrypt it with vault and return the plaintext
 func Decrypt(cipher string) (string, error) {
+	// check if the given cipher is valid
 	if !strings.HasPrefix(cipher, "vault:") {
 		return "", errors.New("Not a vault cipher!")
 	}
 	// make call to vault-ui.io
-	return "", nil
+	client := &http.Client{}
+	payload := strings.NewReader("key=lwu&cipher="+ cipher)
+	req, err := http.NewRequest("POST", "https://vault-ui.io/api/transit/decrypt", payload)
+    if err != nil {
+        log.Print(err)
+    }
+    req.Header.Add("X-Vault-Token", VaultToken)
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+    resp, err := client.Do(req)
+    defer resp.Body.Close()
+    fmt.Println("Status is ",resp.Status)
+    respString, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("Decrypted result: ", string(respString))
+
+    //extract response string
+    stripped := encryptData{}
+    json.Unmarshal([]byte(respString),&stripped)
+    fmt.Println("Decrypted text is: ", stripped.Result)
+	return stripped.Result, nil
 }
