ea6d8409f69c9b537fce9798527c14004632f551
Otto Lau <Otto.Lau@hotmail.com>
2017-07-18 14:06:33 -0700

    Add description of app in home page

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -5,18 +5,28 @@
       <div class="row">
         <div class="Homepage row">
           <div class="col-md-3"></div>
+
           <div class="col-md-6">
-            <div class="Logo-title">
-              <h1>Pair'd</h1>
-              <p>Pair'd is an app that will revolutionize the way you spend your lunch hours!</p>
-              <p>Users create a profile describing their interests and lunch preferences and
-                the app will pair 
-              </p>
+            <div class="row Logo-title">
+              <div class="col-md-4">
+                <img src="../assets/PearLogo.png">
+              </div>
+              <div class="col-md-8">
+                <h1>Pair'd</h1>
+                <p>Pair'd is an app that will revolutionize the way you spend your lunch hours!</p>
+                <p>First, create a profile describing your interests and lunch preferences.  The
+                  app will then pair you with colleagues that share the same tastes!  Pair'd 
+                  also recommends restaurants that both of you would like!  Pair'd makes
+                  arranging lunch outings easy!</p>
+                </p>
+              </div>
             </div>
           </div>
+          
           <div class="col-md-3"></div>
         </div>
       </div>
+      <div class="separator"></div>
   </div>
 </template>
 
@@ -30,12 +40,20 @@ export default {
 <style scoped>
 .Logo-title {
   margin-top: 50px;
-  padding: 10px;
-  background-color: red;
-  border-radius: 5px
+  padding: 20px;
+  background-color: #A66FA6;
+  border-radius: 8px;
+  opacity: 0.9;
+  text-align: center
 }
 .separator {
   background-color:#6F256F;
+  height:125px;
+  width:100%;
+  margin: 0 auto;
+}
+.footer {
+  background-color:#D3D3D3;
   height:50px;
   width:100%;
   margin: 0 auto;
@@ -51,7 +69,11 @@ export default {
   background-color: red;
 }
 h1, h2 {
-  font-weight: normal;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+  text-align: center;
+  color: white
 }
 
 ul {
@@ -66,5 +88,12 @@ li {
 
 a {
   color: #42b983;
+}
+
+p {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 15pt;
+  color: white
 }
 </style>
