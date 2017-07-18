13341279e855a75430f1c8ab5a982c613bceed9c
Otto Lau <Otto.Lau@hotmail.com>
2017-07-18 16:15:45 -0700

    Add contact page background

--- frontend/src/components/contactus.vue
+++ frontend/src/components/contactus.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="ContactUs">
-    <h1>This is theContacts Page</h1>
+  <div>
+    <div class="separator"></div>
+    <div class="ContactUs">
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="heading-title">
+        <h1>Contact Us</h1>
+        </div>
+      </div>
+    </div>
+
+    </div>
+    <div class="separator"></div>
   </div>
 </template>
 
@@ -12,8 +24,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.heading-title {
+  text-align: center;
+  margin-top: 50px
+}
+.ContactUs {
+  background-image: url("../assets/ContactUsBackground.jpeg");
+  background-size: 100% 760px;
+  height: 760px;
+}
+.separator {
+  background-color:#FFF1D6;
+  height:125px;
+  width:100%;
+  margin: 0 auto;
+}
+
 h1, h2 {
-  font-weight: normal;
+  font-weight: initial;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 80px;
+  font-style: italic;
+  color: white
 }
 
 ul {
